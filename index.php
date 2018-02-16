5eef1a0075d1aa8b3fb1f4d01aa0851fcead5f07
macharlaVinod <vinodmacherlamvk@gmail.com>
2018-02-16 10:12:24 +0530

    Signed-off-by: macharlaVinod <vinodmacherlamvk@gmail.com>

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Miron</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+		<link href="font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<div class="wrapper_main">
+			<div class="wrapper">
+				<div class="top_bar">
+					<div class="header">
+						<div class="brand">
+							<h1 class="brand_name">Miro<span>n:</span></h1>
+						</div>
+						<div class="nav_bar">
+							<ul class="menu_bar">
+								<li class="home"><a href="task7.html">HOME</a></li>
+								<li><a href="#">PORTFOLIO<i &nbsp class="fa fa-angle-down" > </i></a>
+									
+									<ul class="dropdown">
+										<hr class="hrow"><li><a href="aboutus.html">PORTFOLIO-1</a></li><hr>
+										<li><a href="#">PORTFOLIO-2</a></li><hr>
+										<li><a href="#">PORTFOLIO-3</a></li><hr>
+										<li><a href="#">PORTFOLIO-4</a></li>
+									</ul>
+									
+								</li>
+								<li><a href="#">BLOG<i class="fa fa-angle-down" ></i></a>
+								
+								<ul class="dropdown">
+										<hr class="hrow"><li><a href="#">BLOG GRID</a></li><hr>
+										<li><a href="#">BLOG SIDEBAR</a></li><hr>
+										<li><a href="#">BLOG FILTER</a></li><hr>
+										<li><a href="#">BLOG POST</a></li>
+									</ul>
+								</li>
+								<li><a href="#">FEATURES<i class="fa fa-angle-down" ></i></a>
+								
+								<ul class="dropdown">
+										<hr class="hrow"><li><a href="#">TYPOGRAPHY</a></li><hr>
+										<li><a href="#">HEADER</a></li><hr>
+										<li><a href="#">SECTION</a></li><hr>
+										<li><a href="#ftr">FOOTER</a></li>
+									</ul>
+								</li>
+								<li><a href="#">PAGES<i class="fa fa-angle-down" ></i></a>
+								
+									<ul class="dropdown">
+										<hr class="hrow"><li><a href="aboutus.html">ABOUT US</a></li><hr>
+										<li><a href="#">CONTACT US</a></li><hr>
+										<li><a href="#">OUR SERVICES</a></li><hr>
+										<li><a href="#">OUR TEAM</a></li><hr>
+										<li><a href="#">GALLERY</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+					
+				<!-- SLIDER -->
+
+				<div class="slider">
+					<div class="bg_image">
+					<img src="images/showcase-1.jpg"  alt="slider-image" />
+					</div>
+				</div>
+				
+				<!-- CONTAINER -->
+				
+				<div class="container_main">
+					<div class="container">
+						<h1 class="sec_head">Miron HTML5 CSS3 fully responsive website template</h1 	>
+						<h2 class="sec_sub">With Clean and Valid Code.</h2>
+						
+						<!-- SECTION -->
+												
+						<div class="section">
+							<div class="sec_1">
+								<i class="fa fa-heart-o" aria-hidden="true"></i>
+								<h3>Make With Love</h3>
+								<p>A new responsive site template! Bright and colorful, with a flexible grid of homepage “featured boxes” so you can like, feature stuff.</p>
+							</div>	
+								
+							<div class="sec_2">
+								<i class="fa fa-bell-o" aria-hidden="true"></i>
+								<h3>Bootsrap3</h3>
+								<p>A new responsive site template! Bright and colorful, with a flexible grid of homepage “featured boxes” so you can like, feature stuff.</p>
+							</div>
+								<div class="sec_2">
+								<i class="fa fa-code" aria-hidden="true"></i>
+								<h3>Clean Code</h3>
+								<p>A new responsive site template! Bright and colorful, with a flexible grid of homepage “featured boxes” so you can like, feature stuff.</p>
+							</div>
+							<div class="sec_4">
+								<i class="fa fa-window-restore" aria-hidden="true"></i>
+								<h3>Fully Responsive</h3>
+								<p>A new responsive site template! Bright and colorful, with a flexible grid of homepage “featured boxes” so you can like, feature stuff.</p>
+							</div>
+						</div>
+					 </div>
+					 <div class="topbtn"> 
+							<a href="#"><i class="fa fa-arrow-circle-up" aria-hidden="true"></a></i>
+					</div>
+				
+				<!-- SECTION 2 -->
+				
+					<div class="section_2">
+						<div class="sec2_head"><h1>Recently Work </h1>
+						<i class="fa fa-file-image-o" aria-hidden="true"></i>
+						</div>
+						<div class="portfilo">
+							<ul class="portfilo_list">
+								<li class="portfilo_all"><a  href="#">all</a></li>
+								<li><a href="#">design</a></li>
+								<li><a href="#">HTML5</a></li>
+								<li><a href="#">Wordpress</a></li>
+								<li><a href="#">seo</a></li>	
+							</ul>
+						</div>
+						
+					</div>
+					
+					<!-- SECTION 3-->
+					
+					<div class="secton_images">
+						<div class="sec_img">
+							
+								<div class="img1"><img src="images/img-1.jpg" alt="bg-1" /></div>
+								<div class="img2"><img src="images/img-2.jpg" alt="bg-2" /></div>
+								<div class="img3"><img src="images/img-3.jpg" alt="bg-3" /></div>
+							
+						</div>
+						<div class="sec_img2">
+						
+								<div class="img4"><img src="images/img-4.jpg" alt="bg-4" /></div>
+								<div class="img5"><img src="images/img-5.jpg" alt="bg-5" /></div>
+								<div class="img6"><img src="images/img-6.jpg" alt="bg-6" /></div>
+								<div class="view_portfilo">
+									<h3 class="viewport"><a href="#">VIEW PORTFOLIO</a></h3>
+								</div>
+						</div>
+					</div>
+					
+					<!-- SECTION 4-->
+					
+					<div class="about_us">
+						<div class="about_head">
+							<h2>About Us</h2>
+							<i class="fa fa-users" aria-hidden="true"></i>
+							
+							<p>Coffee Cols is a creative and amusing magazine-style template that makes uses of boxes on its home page to give your content a unique appearance. Irrespective of the number of boxes you have, they align on their own-so you don’t have to deal with that</p>
+							<div class="socialicons">
+								<ul class="icns">
+								<li><a class="icon" href="LINK" title="Follow with Twitter" target="_blank"><i class="fa fa-facebook"></i></a></li>
+								<!-- <li><a class="icon" href="LINK" title="Follow with Twitter" target="_blank"><i class="fa fa-twitter"></i></a></li> -->
+								<!-- <li><a class="icon" href="LINK" title="Follow with Facebook" target="_blank"><i class="fa fa-facebook"></i></a></li> -->
+								<li><a class="icon" href="LINK" title="Follow with Instagram" target="_blank"><i class="fa fa-instagram"></i></a></li>
+								<li>	<a class="icon" href="LINK" title="Follow with Google Plus" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+								<li>	<a class="icon" href="LINK" title="Follow with Bloglovin" target="_blank"><i class="fa fa-heart"></i></a></li>
+								<li>	<a class="icon" href="LINK" title="Follow with Pinterest" target="_blank"><i class="fa fa-pinterest"></i></a></li>
+								<li>	<a class="icon" href="LINK" title="Follow with YouTube" target="_blank"><i class="fa fa-youtube"></i></a></li>
+								<li>	<a class="icon" href="LINK" title="Follow with Tumblr" target="_blank"><i class="fa fa-tumblr"></i></a></li>
+								<li>	<a class="icon" href="LINK" title="Follow with RSS" target="_blank"><i class="fa fa-rss"></i></a></li>
+								<li>	<a class="icon" href="mailto:LINK" title="Email Me" target="_blank"><i class="fa fa-envelope"></i></a></li>	
+								</ul>
+							</div>
+						</div>
+					</div>
+					
+					<!-- SECTION 5-->
+					
+					<div class="news">
+						<div class="news_head"><h1>Latest News</h1>
+						<i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+						</div>
+							<div class="latest_news">
+								
+									<div class="img1"><img src="images/banner.jpg" alt="bg-1" />
+										<h4>Amazing theme for Multi-Purpose</h4>
+										<p>Appology is a template to be shortly <br>released to creating your brand identity.</p>
+									</div>
+									<div class="img4"><img src="images/img-4.jpg" alt="bg-2" />
+									<h4>Amazing theme for Multi-Purpose</h4>
+										<p>Appology is a template to be shortly <br>released to creating your brand identity.</p>
+									</div>
+									<div class="img2"><img src="images/img-2.jpg" alt="bg-3" />
+									<h4>Amazing theme for Multi-Purpose</h4>
+										<p>Appology is a template to be shortly <br>released to creating your brand identity.</p>
+									</div>
+								
+							</div>
+					</div>		
+				</div>
+				
+				<!-- fOOTER  -->
+				
+				<div class="footer" id="ftr">
+					<div class="footer_menu">
+						<div class="f1">
+							<ul>
+								<h3>About us</h3>
+								<li><p>Vinod Macharla is a web designer and owns a web development company Web Seasoning. He is also an full-time online geek. Follow him on @twitter or sign-up for the email newsletter and get daily updates from this blog.</p></li>
+							</ul>
+						</div>
+						<div class="f2">
+							<h3>Latest post</h3>
+							<ul>
+								<li>Blog slide post</li><hr>
+								<li>Blog image post</li><hr>
+								<li>Blog video post</li><hr>
+								<li>Blog news post</li><hr>
+							</ul>
+						</div>
+						<div class="f3">
+							<h3>Latest work</h3>
+							<ul>
+								<li><img title="Festivus" src="images/img-6.jpg" width="50px"/></li>
+								<li><img title="Sample Bear" src="images/img-5.jpg" width="50px"/></li>
+								<li><img title="Game Plan" src="images/img-4.jpg" width="50px"/></li>
+								<li><img title="Clear" src="images/img-3.jpg" width="50px"/></li>
+								<li><img title="Pen fest" src="images/img-2.jpg" width="50px"/></li>
+								<li><img title="Festivus" src="images/img-1.jpg" width="50px"/></li>
+								
+							</ul>
+						</div>
+						<div class="f4">
+							<h3>Get in touch</h3>
+							<ul>
+								<li><i class="fa fa-home" aria-hidden="true"></i> &nbsp B-16,Madura Nagar,Hyderabad 50008</li>
+								<li><i class="fa fa-phone" aria-hidden="true"></i> &nbsp Phone : +91 9008814366</li>
+								<li><i class="fa fa-envelope" aria-hidden="true"></i> &nbsp Email : mail@marin.com</li>
+								<li><i class="fa fa-globe" aria-hidden="true"></i> &nbsp Website : http://www.mvtricks.com</li>
+							</ul>
+						</div>
+						<div class="ftitile">
+								<h1 class="fbrand">Miro<span>n:</span></h1>
+						
+						</div>
+						<div class="crights">
+								<h5 class="cr">© copyright 2018 - All rights reserved.</h5>
+										<marquee behavior="scroll" direction="left">
+											Thanq visit again
+										</marquee>
+
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+<html>
